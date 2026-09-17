806da29d20435f6c356a38eefb0cf38d256fd2f7
Anda Adriana Achimescu <anda.a@live.ca>
2026-09-16 21:33:31 -0700

    First commit

---
+++ spendi/spendiApp.swift
@@ -0,0 +1,17 @@
+//
+//  spendiApp.swift
+//  spendi
+//
+//  Created by Anda Achimescu on 2026-09-16.
+//
+
+import SwiftUI
+
+@main
+struct spendiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ spendiTests/spendiTests.swift
@@ -0,0 +1,16 @@
+//
+//  spendiTests.swift
+//  spendiTests
+//
+//  Created by Anda Achimescu on 2026-09-16.
+//
+
+import Testing
+
+struct spendiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
